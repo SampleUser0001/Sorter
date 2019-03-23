4afd15f11fdc2419157048fa5d82c96a9dfeef61
Satoru Tanaka <ittimfn@gmail.com>
2019-03-23 23:00:55 +0900

    First commit.

---
+++ src/tool/sort/Sorter.java
@@ -0,0 +1,61 @@
+package tool.sort;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Sorter {
+
+	/** ソート条件を記載しているファイル */
+	public static final Path SORT_CONDITION = Paths.get(System.getProperty("user.dir"),"files","sort.txt");
+
+	/** ソート対象ファイル */
+	public static final Path INPUT = Paths.get(System.getProperty("user.dir"),"files","input.tsv");
+
+	/** ソート結果ファイル */
+	public static final Path OUTPUT = Paths.get(System.getProperty("user.dir"),"files","output.tsv");
+
+	public static final String SEPARATOR = "\t";
+
+	public static void main(String[] args) throws IOException{
+
+
+		// ソート対象ファイル読み込み。
+		// ソート条件に引っかからなかった値は元の順序のまま出力したいのでLinkedHashMapを使用。
+		Map<String,String> inputMap = new LinkedHashMap<String, String>();
+		Files.lines(INPUT).forEach(
+				line -> inputMap.put(line.split(SEPARATOR)[0], line));
+
+		try(BufferedWriter writer = Files.newBufferedWriter(OUTPUT, StandardOpenOption.CREATE)){
+
+			// ソート条件ファイルの順に結果ファイルに書き込む。
+			Files.readAllLines(SORT_CONDITION).forEach( key -> {
+				try {
+					if(inputMap.containsKey(key)){
+						writer.write(inputMap.get(key) + System.lineSeparator());
+						// 出力した値は消す。
+						inputMap.remove(key);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+
+			// 残っている値を出力。
+			inputMap.values().forEach( value -> {
+				try {
+					writer.write(value + System.lineSeparator());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+		}
+
+
+	}
+}
